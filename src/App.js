515aabb1b66f0af4db4df95299f8b786ecf796ec
Harsh Singh <harshsinghujjain07@gmail.com>
2023-11-11 14:41:16 +0530

    cart page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-
+import CartPage from './pages/CartPage';
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom";
+
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 
